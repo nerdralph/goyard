a5ea4bad35302e0b3e6168a982bda411de9c8c45
nerdralph <ralphdoncaster@gmail.com>
2018-03-03 18:12:52 -0400

    switch from int32 to uint32

--- prime_test.go
+++ prime_test.go
@@ -6,12 +6,12 @@ import (
 	"math/big"
 )
 
-func i32Prime(n int32) bool {
+func i32Prime(n uint32) bool {
 //    if (n==2)||(n==3) {return true;}
     if n%2 == 0 { return false }
     if n%3 == 0 { return false }
-    sqrt := int32(math.Sqrt(float64(n)))
-    for i := int32(5); i <= sqrt; i+=6 {
+    sqrt := uint32(math.Sqrt(float64(n)))
+    for i := uint32(5); i <= sqrt; i+=6 {
         if n%i == 0 { return false }
         if n%(i+2) == 0 { return false; }
     }
@@ -23,7 +23,7 @@ const num = 1 << 19 -1
 //const num = 2017133
 
 func TestPrime(t *testing.T){
-	prime := int32(num)
+	prime := uint32(num)
 	if !i32Prime(prime){
 		t.Error(prime, "failed prime test")
 	}
@@ -37,7 +37,7 @@ func TestPPrime(t *testing.T){
 }
 
 func BenchmarkPrime(b *testing.B){
-	prime := int32(num)
+	prime := uint32(num)
 	for n :=0; n < b.N; n++ {
 		i32Prime(prime)
 	}
