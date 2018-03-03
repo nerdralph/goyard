c0c31b302bcd6fc0819baae5fa73840d52340909
Nerd Ralph <ralphdoncaster@gmail.com>
2018-03-03 21:23:34 +0000

    use const for number to test

--- prime_test.go
+++ prime_test.go
@@ -3,10 +3,11 @@ package main
 import (
     "math"
     "testing"
+	"math/big"
 )
 
-func isPrime(n int32) bool {
-    if (n==2)||(n==3) {return true;}
+func i32Prime(n int32) bool {
+//    if (n==2)||(n==3) {return true;}
     if n%2 == 0 { return false }
     if n%3 == 0 { return false }
     sqrt := int32(math.Sqrt(float64(n)))
@@ -17,16 +18,27 @@ func isPrime(n int32) bool {
     return true
 }
 
+//const num = 65533051
+const num = 1 << 19 -1
+//const num = 2017133
+
 func TestPrime(t *testing.T){
-	prime := int32(65533051)
-	if !isPrime(prime){
+	prime := int32(num)
+	if !i32Prime(prime){
 		t.Error(prime, "is actually prime")
 	}
 }
 
 func BenchmarkPrime(b *testing.B){
-	prime := int32(65533051)
-	for i :=0; i< b.N; i++ {
-		isPrime(prime)
+	prime := int32(num)
+	for n :=0; n < b.N; n++ {
+		i32Prime(prime)
+	}
+}
+
+func BenchmarkPPrime(b *testing.B){
+	prime := big.NewInt(num)
+	for n :=0; n < b.N; n++ {
+		prime.ProbablyPrime(0)
 	}
 }
